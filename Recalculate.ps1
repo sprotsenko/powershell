1142e88acc030df311017126b189f87121e139a4
sprotsenko <54120679+sprotsenko@users.noreply.github.com>
2023-01-05 00:26:59 +0200

    Update Recalculate.ps1
    working script

--- Recalculate.ps1
+++ Recalculate.ps1
@@ -1,5 +1,5 @@
 $linesNumber = 0
-$filePath = 'C:\Working Documentation\recalculate.txt'
+$filePath = 'E:\recalculate.txt'
 $numberOfIterations = [Math]::Ceiling((Get-Content -Path $filePath).Length/400)
 
 for($i=1; $i -le $numberOfIterations; $i++){
@@ -16,12 +16,11 @@ foreach($barcode in $barcodeSet){
     $body = $body.Substring(0,$body.Length-1)
     $body = $body + "]"
 
-
 # Make a recalculation API request
 try {
 
-    $req = Invoke-RestMethod -Method PUT -Uri http://10.255.102.215:8080/UkrPostAPI/shipments/recalculation/barcode?token=debf2025-78c6-42ea-b120-21dce332ac15 -Body ($body)
-    $req   
+    $req = Invoke-RestMethod -Method PUT -Uri http://10.255.102.215:8080/UkrPostAPI/shipments/recalculation/barcode?token=debf2025-78c6-42ea-b120-21dce332ac15 -Body ($body) -ContentType application/json
+    Write-Host "$i"
 }
 catch {
     throw "Unnable to recalculate the provided barcode list"
